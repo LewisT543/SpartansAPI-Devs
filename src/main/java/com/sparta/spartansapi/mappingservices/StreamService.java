cf0b0855968a9db7696be1865fdd0a6e0ada3143
TobiOl <1712264@brunel.ac.uk>
2021-11-16 14:58:01 +0000

    update streams added

--- src/main/java/com/sparta/spartansapi/mappingservices/StreamService.java
+++ src/main/java/com/sparta/spartansapi/mappingservices/StreamService.java
@@ -4,9 +4,17 @@ import com.sparta.spartansapi.dtos.StreamDTO;
 import com.sparta.spartansapi.mongodb.models.Stream;
 import com.sparta.spartansapi.mongodb.repos.StreamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,5 +39,20 @@ public class StreamService {
         streamDTO.setStream_name(stream.getStream_name());
         streamDTO.setStream_duration(stream.getStream_duration());
         return streamDTO;
+    }
+
+
+    public ResponseEntity<Stream> updateStream(String id, Stream stream){
+        Optional<Stream> streamData = streamRepository.findById(id);
+        if (streamData.isPresent()){
+            Stream _stream = streamData.get();
+            _stream.setStream_name(stream.getStream_name());
+            _stream.setStream_duration(stream.getStream_duration());
+            return new ResponseEntity<>(streamRepository.save(_stream), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
