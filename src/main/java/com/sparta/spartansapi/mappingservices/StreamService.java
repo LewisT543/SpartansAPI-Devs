77d7ed66b660c69205368af3bce31f4ccb399349
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2021-11-18 11:39:33 +0000

    Updated Error message handling and API response format

--- src/main/java/com/sparta/spartansapi/mappingservices/StreamService.java
+++ src/main/java/com/sparta/spartansapi/mappingservices/StreamService.java
@@ -1,9 +1,13 @@
 package com.sparta.spartansapi.mappingservices;
 
 
-import com.sparta.spartansapi.mongodb.models.Spartan;
 import com.sparta.spartansapi.mongodb.models.Stream;
 import com.sparta.spartansapi.mongodb.repos.StreamRepository;
+import com.sparta.spartansapi.utils.APIMessageResponse;
+import com.sparta.spartansapi.utils.APIResponse;
+import com.sparta.spartansapi.utils.ResponseManager;
+import io.swagger.annotations.Api;
+import io.swagger.models.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,22 +29,23 @@ public class StreamService {
 
     public ResponseEntity<?> addStream(Stream stream) {
         try {
-            return new ResponseEntity<>(streamRepository.insert(stream), HttpStatus.OK);
+            return new ResponseEntity<>(new APIResponse(new ArrayList<>(List.of(streamRepository.insert(stream))), ResponseManager.RECORD_ADDED, 1, HttpStatus.OK.value()), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Unexpected Server error", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new APIMessageResponse(ResponseManager.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     public ResponseEntity<?> findAllStreams() {
         try {
             List<Stream> streams = new ArrayList<>(streamRepository.findAll());
-            if(streams.isEmpty())
-                return new ResponseEntity<>("No Streams found",  HttpStatus.OK);
-            return new ResponseEntity<>(streams, HttpStatus.OK);
+            if(streams.isEmpty()) {
+                return new ResponseEntity<>(new APIResponse(streams, ResponseManager.NO_RECORDS_FOUND,0, HttpStatus.OK.value()),  HttpStatus.OK);
+            }
+            return new ResponseEntity<>(new APIResponse(streams, ResponseManager.RECORDS_FOUND, streams.size(), HttpStatus.OK.value()), HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Unexpected Server error.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new APIMessageResponse(ResponseManager.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -48,27 +53,26 @@ public class StreamService {
         try {
             List<Stream> streams = streamRepository.getStreamsByNameContains(name);
             if (streams.isEmpty())
-                return new ResponseEntity<>("No Streams found: " + name, HttpStatus.OK);
-            return new ResponseEntity<>(streams, HttpStatus.OK);
+                return new ResponseEntity<>(new APIResponse(streams, ResponseManager.NO_RECORDS_FOUND,0, HttpStatus.OK.value()), HttpStatus.OK);
+            return new ResponseEntity<>(new APIResponse(streams, ResponseManager.RECORDS_FOUND, streams.size(), HttpStatus.OK.value()), HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity<>("Unexpected Server error.",HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new APIMessageResponse(ResponseManager.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     public ResponseEntity<?> updateStream(String id, Stream stream){
         try {
             Optional<Stream> streamData = streamRepository.findById(id);
-            if (streamData.isPresent()){
+            if (streamData.isPresent()) {
                 Stream _stream = streamData.get();
                 _stream.setName(stream.getName());
                 _stream.setDuration(stream.getDuration());
-                return new ResponseEntity<>(streamRepository.save(_stream), HttpStatus.OK);
-            }
-            else
-                return new ResponseEntity<>("Stream not found",HttpStatus.OK);
+                return new ResponseEntity<>(new APIResponse(new ArrayList<>(List.of(streamRepository.save(_stream))), ResponseManager.RECORD_ADDED, 1, HttpStatus.OK.value()), HttpStatus.OK);
+            } else
+                return new ResponseEntity<>(new APIMessageResponse(ResponseManager.NO_RECORD_FOUND),HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
-            return new ResponseEntity<>("Unexpected Server error.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new APIMessageResponse(ResponseManager.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -77,11 +81,11 @@ public class StreamService {
             Optional<Stream> foundStream = streamRepository.findById(id);
             if (foundStream.isPresent()) {
                 streamRepository.deleteById(id);
-                return new ResponseEntity<>("Stream has been deleted: "+ id, HttpStatus.OK);
+                return new ResponseEntity<>(new APIMessageResponse(ResponseManager.RECORD_DELETED), HttpStatus.OK);
             } else
-                return new ResponseEntity<>("Cannot delete Stream with id: " + id, HttpStatus.OK);
+                return new ResponseEntity<>(new APIMessageResponse(ResponseManager.NO_RECORD_FOUND),HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>("Unexpected Server error.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new APIMessageResponse(ResponseManager.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
